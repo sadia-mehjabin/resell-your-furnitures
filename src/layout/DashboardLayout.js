a774b91e2585de99cee72393e37b5a76eb77ad02
sadia-mehjabin <jhidnicraft@gmail.com>
2022-11-26 00:00:20 +0600

    role based link added

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -1,8 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Header from '../shared/Header';
 
 const DashboardLayout = () => {
+    const { data: usersRole = [], isLoading, refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/usersRole');
+            const data = await res.json()
+            return data;
+        }
+    })
+
+    // const role = usersRole.map(rol => console.log(rol.role))
+
     return (
         <div>
             <Header></Header>
@@ -16,9 +28,23 @@ const DashboardLayout = () => {
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-48 bg-base-100 text-base-content">
-                       
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+
+                        <li><Link>My orders</Link></li>
+                        {
+                            usersRole.map(rol => {
+                                if (rol.role === 'seller') {
+                                    return <>
+                                        <li><Link>Add a product</Link></li>
+                                        <li><Link>My products</Link></li>
+                                        <li><Link>My buyers</Link></li>
+                                    </>
+                                }
+                            })
+                        }
+                        <li><Link>All users</Link></li>
+                        <li><Link>All buyers</Link></li>
+                        <li><Link>Reported items</Link></li>
+
                     </ul>
 
                 </div>
